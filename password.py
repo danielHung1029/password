21665d631778334eabe91e497ae7422664edadc2
Daniel Hung <see98524@gmail.com>
2019-01-27 00:20:51 +0800

    add precise solution2

--- password.py
+++ password.py
@@ -27,9 +27,24 @@
 # 					print('登入成功')
 # 					break
 
+# #Solution 1
+# password = 'a123456'
+# chance = 3
+# while True:
+# 	pwd = input('請輸入密碼: ')
+# 	if pwd == password:
+# 		print('登入成功')
+# 		break
+# 	else:
+# 		chance = chance - 1
+# 		print('密碼錯誤！ 還有', chance, '次機會')
+# 		if chance == 0:
+# 			break
+
+#Solution 2
 password = 'a123456'
 chance = 3
-while True:
+while chance > 0:
 	pwd = input('請輸入密碼: ')
 	if pwd == password:
 		print('登入成功')
@@ -37,5 +52,3 @@ while True:
 	else:
 		chance = chance - 1
 		print('密碼錯誤！ 還有', chance, '次機會')
-		if chance == 0:
-			break
