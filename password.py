5f6b941af024f692b4d7e6ab84600be6dfd714d0
Daniel Hung <see98524@gmail.com>
2019-01-27 00:11:36 +0800

    add precise solution

--- password.py
+++ password.py
@@ -1,29 +1,41 @@
+# password = 'a123456'
+# chance = 3
+# while True:
+# 	pwd = input('請輸入密碼: ')
+# 	if pwd == password:
+# 		print('登入成功')
+# 		break
+# 	elif pwd != password:
+# 		chance = chance - 1
+# 		print('密碼錯誤！ 還有', chance, '次機會')
+# 		pwd = input('請輸入密碼: ')
+# 		if pwd == password:
+# 			print('登入成功')
+# 			break
+# 		elif pwd != password:
+# 			chance = chance - 1
+# 			print('密碼錯誤！ 還有', chance, '次機會')
+# 			pwd = input('請輸入密碼: ')
+# 			if pwd == password:
+# 				print('登入成功')
+# 				break
+# 			elif pwd != password:
+# 				chance = chance - 1
+# 				print('密碼錯誤！ 還有', chance, '次機會')
+# 				break
+# 				if pwd == password:
+# 					print('登入成功')
+# 					break
+
+password = 'a123456'
 chance = 3
 while True:
 	pwd = input('請輸入密碼: ')
-	if pwd == 'a123456':
+	if pwd == password:
 		print('登入成功')
 		break
-	elif pwd != 'a123456':
-		print('密碼錯誤！ 還有', chance -1, '次機會')
+	else:
 		chance = chance - 1
-		pwd = input('請輸入密碼: ')
-		if pwd == 'a123456':
-			print('登入成功')
+		print('密碼錯誤！ 還有', chance, '次機會')
+		if chance == 0:
 			break
-		elif pwd != 'a123456':
-			print('密碼錯誤！ 還有', chance -1, '次機會')
-			chance = chance - 1
-			pwd = input('請輸入密碼: ')
-			if pwd == 'a123456':
-				print('登入成功')
-				break
-			elif pwd != 'a123456':
-				print('密碼錯誤！ 還有', chance -1, '次機會')
-				chance = chance - 1
-				break
-				if pwd == 'a123456':
-					print('登入成功')
-					break
-
-
